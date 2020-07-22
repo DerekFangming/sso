143ed71dec7cbcb57a50bbc860895650468b13f2
synfm@126.com <synfm@126.com>
2020-07-21 23:34:53 -0500

    Allow war file

--- src/main/resources/createTable.sql
+++ src/main/resources/createTable.sql
@@ -8,3 +8,13 @@ create table sso_users (
 	password_reset_code text,
 	created_at timestamp without time zone not null
 );
+
+create table sso_client_details (
+	client_id text primary key,
+	client_secret text,
+	scope text,
+	authorized_grant_types text,
+	redirect_uri text,
+	access_token_validity_seconds integer,
+    refresh_token_validity_seconds integer
+);
