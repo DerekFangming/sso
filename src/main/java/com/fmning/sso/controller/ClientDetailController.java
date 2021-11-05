e0104043a1f70252a91b12d55dbaae596e050f5a
synfm@126.com <synfm@126.com>
2021-11-04 23:09:41 -0500

    Update client tokens

--- src/main/java/com/fmning/sso/controller/ClientDetailController.java
+++ src/main/java/com/fmning/sso/controller/ClientDetailController.java
@@ -10,11 +10,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -22,7 +20,6 @@ import java.util.UUID;
 @RequestMapping("/client-details")
 public class ClientDetailController {
 
-//    private final UserRepo userRepo;
     private final ClientDetailRepo clientDetailRepo;
     private final PasswordService passwordService;
 
@@ -55,6 +52,31 @@ public class ClientDetailController {
         clientDetailRepo.save(clientDetail);
 
         clientDetailDto.setClientSecret(clientSecret);
+        return ResponseEntity.ok(clientDetailDto);
+    }
+
+    @PutMapping("/{clientId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ClientDetailDto> updateClientDetail(@PathVariable(value="clientId") String clientId, @RequestBody ClientDetailDto clientDetailDto) {
+        if (clientDetailDto.getAccessTokenValiditySeconds() < 60 || clientDetailDto.getRefreshTokenValiditySeconds() < 60) {
+            throw new IllegalArgumentException("Token validity time has to be greater than 60 (1 min).");
+        } else if (StringUtils.isEmpty(clientDetailDto.getAuthorizedGrantTypes())) {
+            throw new IllegalArgumentException("Grant type cannot be empty.");
+        }
+
+        Optional<ClientDetail> clientDetailOptional = clientDetailRepo.findById(clientId);
+        if (!clientDetailOptional.isPresent()) {
+            throw new IllegalArgumentException("Client ID does not exist: " + clientId);
+        }
+
+        ClientDetail clientDetail = clientDetailOptional.get();
+        clientDetail.setAuthorizedGrantTypes(clientDetailDto.getAuthorizedGrantTypes());
+        clientDetail.setRedirectUri(clientDetailDto.getRedirectUri());
+        clientDetail.setAccessTokenValiditySeconds(clientDetailDto.getAccessTokenValiditySeconds());
+        clientDetail.setRefreshTokenValiditySeconds(clientDetailDto.getRefreshTokenValiditySeconds());
+
+        clientDetailRepo.save(clientDetail);
+
         return ResponseEntity.ok(clientDetailDto);
     }
 }
