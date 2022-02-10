ec26d172215102490a78ac30ac69e4b927d4043e
synfm@126.com <synfm@126.com>
2022-02-10 08:45:05 -0600

    sso logging

--- src/main/java/com/fmning/sso/config/SsoExceptionHandler.java
+++ src/main/java/com/fmning/sso/config/SsoExceptionHandler.java
@@ -4,21 +4,25 @@ import com.fmning.sso.dto.BaseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@CommonsLog
 @ControllerAdvice
 public class SsoExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<BaseDto> handleException(Exception e) {
         if (e instanceof IllegalArgumentException) {
+            log.error("Invalid request", e);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(e.getMessage()));
         }
 
+        log.error("Unhandled exception", e);
         e.printStackTrace();
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDto("Internal server error"));
